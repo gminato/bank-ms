f3fb3be06d818dc2aaabb4ac0858394ca6235991
Harshit <harshit.3851@gmail.com>
2024-09-14 21:34:46 +0530

    added open api documentation of apis

--- src/main/java/monkey/luffy/accounts/AccountsApplication.java
+++ src/main/java/monkey/luffy/accounts/AccountsApplication.java
@@ -4,8 +4,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditorAware")
+@OpenAPIDefinition(
+	info = @io.swagger.v3.oas.annotations.info.Info(
+		title = "Accounts API",
+		version = "1.0",
+		description = "Accounts API",
+		contact = @io.swagger.v3.oas.annotations.info.Contact(
+			name = "Harshit Gupta",
+			email = "harshit.3851@gmail.com"
+		)
+	)
+)
 public class AccountsApplication {
 
 	public static void main(String[] args) {
